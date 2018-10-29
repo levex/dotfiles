97250bbb04d581dbe72f0c6e207c17551b231457
Levente Kurusa <lkurusa@acm.org>
2018-10-29 23:53:17 +0200

    bin: add lock.sh for locking the screen
    Signed-off-by: Levente Kurusa <lkurusa@acm.org>

---
+++ bin/lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DISPLAY=:0
+PICTURE=/tmp/i3lock.png
+SCREENSHOT="scrot $PICTURE"
+
+BLUR="8x8"
+
+$SCREENSHOT
+convert $PICTURE -blur $BLUR $PICTURE
+i3lock -i $PICTURE
+rm $PICTURE
