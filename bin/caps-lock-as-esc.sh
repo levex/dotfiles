8e9ca139e88d27e01e9ab76e401c2c0784f1a17e
Levente Kurusa <lkurusa@acm.org>
2019-01-03 15:13:33 +0100

    update caps-lock-as-esc script
    Signed-off-by: Levente Kurusa <lkurusa@acm.org>

--- bin/caps-lock-as-esc.sh
+++ bin/caps-lock-as-esc.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 CAPS_LOCK=66
+RIGHT_CTRL=105
 
 # Caps_Lock = Escape, Shift + Caps_Lock = Caps_Lock
 xmodmap -e 'remove Lock = Caps_Lock'
 xmodmap -e "keycode $CAPS_LOCK = Escape Caps_Lock"
+xmodmap -e "keycode $RIGHT_CTRL = Multi_key"
